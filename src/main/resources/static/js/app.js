a88a4393bb9ff5715573ab34ccd538f6b4513610
Ramesh Raju Alluri <g801691@ad.syniverse.com>
2017-03-31 17:45:59 +0530

    added static security module

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,11 +1,45 @@
 var app=angular.module('myApp',[ ]);
+app.config(function($httpProvider){
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+});
+
 app.controller('myCtrl',myCtrl);
 
 myCtrl.$inject=['$scope','$http'];
 function myCtrl($scope,$http){
+$scope.authenticated=false;
+$scope.login=function(){
+    var headers =  {
+					authorization : "Basic "
+							+ btoa("user:ramesh")
+} ;
+$http.get('/user/', {
+					headers : headers
+				}).success(function(response) {
+                                    console.log(" ::" +angular.toJson(response));
+//					if (response.data.name) {
+						$scope.authenticated = true;
+//					} else {
+//						$scope.authenticated = false;
+//					}
+//					callback && callback($scope.authenticated);
+				},function(error){
+                                     console.log(" ::" +angular.toJson(error));
+                                    $scope.authenticated = false;
+                                });
+}
 
+$scope.getresponse=function(){
  $http.get('/resource/').success(function(data) {
     $scope.greeting = data;
   });
+};
+$scope.getresponse();
+$scope.logout=function(){
+ $http.post('logout', {}).finally(function() {
+					$scope.authenticated = false;
+					$location.path("/");
+});
+};
 }
    
